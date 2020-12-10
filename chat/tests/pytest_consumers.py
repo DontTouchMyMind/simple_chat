d18f12c1ef3d8447f3bd8c19a7cc9e12443c2b19
DontTouchMyMind <tobigface@gmail.com>
2020-12-10 14:26:08 +0200

    Added tests for consumers.

---
+++ chat/tests/pytest_consumers.py
@@ -0,0 +1,67 @@
+import pytest
+
+from channels.db import database_sync_to_async
+from channels.testing import WebsocketCommunicator
+from django.test import Client
+
+
+from chat.models import User
+from simple_chat.routing import application
+
+
+@database_sync_to_async
+def create_user(username, email, password):
+    user = User.objects.create_user(
+        username=username,
+        email=email,
+        password=password
+    )
+    return user
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def pytest_consumers():
+    client = Client()
+    new_user = await create_user('test_p_1', 'test_p_1@gmail.com', 'QAZ1598753')
+    client.force_login(user=new_user)
+
+    communicator = WebsocketCommunicator(
+        application=application,
+        path='/ws/groups/',
+        headers=[(
+            b'cookie',
+            f'sessionid={client.cookies["sessionid"].value}'.encode('ascii')
+        )]
+    )
+    connected, _ = await communicator.connect()
+    # Connection test.
+    assert connected
+
+    # Group creation test.
+    await communicator.send_json_to({'event': 'group.create', 'data': {'name': 'Group 1'}})
+    result = await communicator.receive_json_from()
+    assert result['status'] == 'ok'
+    assert result['event'] == 'group.create'
+    assert result['data']['name'] == 'Group 1'
+
+    # Test parse content.
+    await communicator.send_json_to({'event': '', 'data': ''})  # 'data' is dict.
+    result = await communicator.receive_json_from()
+    assert result['status'] == 'error'
+    assert result['data']['detail'] == 'Invalid message!'
+
+    # Test method undefined.
+    await communicator.send_json_to({'event': 'undefined.method', 'data': {}})
+    result = await communicator.receive_json_from()
+    assert result['status'] == 'error'
+    assert result['event'] == 'undefined.method'
+    assert result['data']['detail'] == 'Unknown event'
+
+    await communicator.send_json_to({'event': 'group.list', 'data': {}})
+    result = await communicator.receive_json_from()
+    assert result['status'] == 'ok'
+    assert result['event'] == 'group.list'
+    assert len(result['data']) == 1
+
+    await communicator.disconnect()
--- chat/tests/pytest_tests.py
+++ chat/tests/pytest_tests.py
@@ -1,27 +1,17 @@
 import pytest
-from django.test import Client
 
 from chat.models import User
 
 
-def create_user(username, email, password):
-    user = User.objects.create_user(
-        username=username,
-        email=email,
-        password=password
-    )
-    return user
-
-
 @pytest.mark.django_db
 def pytest_creating_users():
-    new_user = create_user('test_p_1', 'test_p_1@gmail.com', 'QAZ1598753')
+    new_user = User.objects.create_user(username='test_p_1', email='test_p_1@gmail.com', password='QAZ1598753')
     assert User.objects.count() == 1
 
 
 @pytest.mark.django_db
 def pytest_banned_user():
-    new_user = create_user('test_p_1', 'test_p_1@gmail.com', 'QAZ1598753')
+    new_user = User.objects.create_user(username='test_p_1', email='test_p_1@gmail.com', password='QAZ1598753')
     assert new_user.user_status == 'a'
 
     new_user.user_status = 'b'
