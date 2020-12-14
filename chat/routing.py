46096741cd308014b23a633a6e6b770244a06756
DontTouchMyMind <tobigface@gmail.com>
2020-12-14 11:21:46 +0200

    Added new route for chat.

--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
 
-from .consumers import BaseChatConsumer, GroupChatConsumer
+from .consumers import BaseChatConsumer, GroupChatConsumer, ChatConsumer
 
 websocket_urlpatterns = [
     re_path(r'^ws/groups/$', GroupChatConsumer),
+    re_path(r'^ws/chat/(?P<group_id>\d+)/$', ChatConsumer),
 ]
