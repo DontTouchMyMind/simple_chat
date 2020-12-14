179823f1e0192255c8162c232d0e1bf2e554c922
DontTouchMyMind <tobigface@gmail.com>
2020-12-14 16:37:12 +0200

    Added route.

--- chat/routing.py
+++ chat/routing.py
@@ -1,8 +1,8 @@
 from django.urls import re_path
 
-from .consumers import BaseChatConsumer, GroupChatConsumer, ChatConsumer
+from .consumers import GroupChatConsumer, ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'^ws/groups/$', GroupChatConsumer),
-    re_path(r'^ws/chat/(?P<group_id>\d+)/$', ChatConsumer),
+    re_path(r'ws/groups/$', GroupChatConsumer),
+    re_path(r'ws/chat/(?P<group_id>\d+)/$', ChatConsumer),
 ]
