090fffe315b10c8218bce2cda0126ecbd3f8ea01
DontTouchMyMind <tobigface@gmail.com>
2021-02-05 14:10:19 +0200

    Added path for test_room.html.

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from chat.views import LoginView, SignupView, index, logout_user
+from chat.views import LoginView, SignupView, index, logout_user, room
 
 urlpatterns = [
     path('', index),
     path('login/', LoginView.as_view()),
     path('logout/', logout_user),
     path('signup/', SignupView.as_view()),
+    path('chat/<str:group_id>/', room, name='test_room')
 ]
