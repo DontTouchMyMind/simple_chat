ca5e7f72c9cf7e058e4d9140bcea93dadacb8b23
DontTouchMyMind <tobigface@gmail.com>
2021-02-05 14:11:32 +0200

    Fixed link for groups.

--- chat/models.py
+++ chat/models.py
@@ -19,8 +19,8 @@ class ChatGroup(models.Model):
     @property
     def link(self):
         channel_name = self.channel_name(self.id)
-        # return f'/ws/chat/{self.id}'
-        return f'/ws/chat/{channel_name}/'
+        return f'/ws/chat/{self.id}'
+        # return f'/ws/chat/{channel_name}/'
 
     def __str__(self):
         return self.name
